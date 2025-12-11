75a3ac8199c5cdf4ca0992f3f40b29515e26f75e
Amir Hasanpour <amirhasanpour.hp10@gmail.com>
2025-12-12 01:20:52 +0330

    Create user client for gateway service

---
+++ api-gateway/internal/client/user_client.go
@@ -0,0 +1,151 @@
+package client
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	pb "github.com/amirhasanpour/task-manager/api-gateway/proto"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/trace"
+	"go.uber.org/zap"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+type UserClient interface {
+	CreateUser(ctx context.Context, req *pb.CreateUserRequest) (*pb.CreateUserResponse, error)
+	GetUser(ctx context.Context, req *pb.GetUserRequest) (*pb.GetUserResponse, error)
+	UpdateUser(ctx context.Context, req *pb.UpdateUserRequest) (*pb.UpdateUserResponse, error)
+	DeleteUser(ctx context.Context, req *pb.DeleteUserRequest) (*pb.DeleteUserResponse, error)
+	ListUsers(ctx context.Context, req *pb.ListUsersRequest) (*pb.ListUsersResponse, error)
+	Register(ctx context.Context, req *pb.RegisterRequest) (*pb.RegisterResponse, error)
+	Login(ctx context.Context, req *pb.LoginRequest) (*pb.LoginResponse, error)
+	ValidateToken(ctx context.Context, req *pb.ValidateTokenRequest) (*pb.ValidateTokenResponse, error)
+	Close() error
+}
+
+type userClient struct {
+	conn   *grpc.ClientConn
+	client pb.UserServiceClient
+	logger *zap.Logger
+	tracer trace.Tracer
+}
+
+type UserConfig struct {
+	Host    string
+	Port    int
+	Timeout time.Duration
+}
+
+func NewUserClient(cfg UserConfig) (UserClient, error) {
+	address := fmt.Sprintf("%s:%d", cfg.Host, cfg.Port)
+	
+	conn, err := grpc.NewClient(
+		address,
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+		grpc.WithTimeout(cfg.Timeout),
+	)
+	if err != nil {
+		return nil, fmt.Errorf("failed to connect to user service: %w", err)
+	}
+
+	client := pb.NewUserServiceClient(conn)
+	
+	logger := zap.L().Named("user_client")
+	logger.Info("Connected to user service", zap.String("address", address))
+
+	return &userClient{
+		conn:   conn,
+		client: client,
+		logger: logger,
+		tracer: otel.Tracer("user-client"),
+	}, nil
+}
+
+func (c *userClient) CreateUser(ctx context.Context, req *pb.CreateUserRequest) (*pb.CreateUserResponse, error) {
+	ctx, span := c.tracer.Start(ctx, "UserClient.CreateUser")
+	defer span.End()
+
+	span.SetAttributes(
+		attribute.String("user.email", req.Email),
+		attribute.String("user.username", req.Username),
+	)
+
+	c.logger.Debug("Creating user", zap.String("email", req.Email))
+	return c.client.CreateUser(ctx, req)
+}
+
+func (c *userClient) GetUser(ctx context.Context, req *pb.GetUserRequest) (*pb.GetUserResponse, error) {
+	ctx, span := c.tracer.Start(ctx, "UserClient.GetUser")
+	defer span.End()
+
+	span.SetAttributes(attribute.String("user.id", req.Id))
+	c.logger.Debug("Getting user", zap.String("id", req.Id))
+	return c.client.GetUser(ctx, req)
+}
+
+func (c *userClient) UpdateUser(ctx context.Context, req *pb.UpdateUserRequest) (*pb.UpdateUserResponse, error) {
+	ctx, span := c.tracer.Start(ctx, "UserClient.UpdateUser")
+	defer span.End()
+
+	span.SetAttributes(attribute.String("user.id", req.Id))
+	c.logger.Debug("Updating user", zap.String("id", req.Id))
+	return c.client.UpdateUser(ctx, req)
+}
+
+func (c *userClient) DeleteUser(ctx context.Context, req *pb.DeleteUserRequest) (*pb.DeleteUserResponse, error) {
+	ctx, span := c.tracer.Start(ctx, "UserClient.DeleteUser")
+	defer span.End()
+
+	span.SetAttributes(attribute.String("user.id", req.Id))
+	c.logger.Debug("Deleting user", zap.String("id", req.Id))
+	return c.client.DeleteUser(ctx, req)
+}
+
+func (c *userClient) ListUsers(ctx context.Context, req *pb.ListUsersRequest) (*pb.ListUsersResponse, error) {
+	ctx, span := c.tracer.Start(ctx, "UserClient.ListUsers")
+	defer span.End()
+
+	span.SetAttributes(
+		attribute.Int("page", int(req.Page)),
+		attribute.Int("page_size", int(req.PageSize)),
+	)
+	c.logger.Debug("Listing users", zap.Int32("page", req.Page), zap.Int32("page_size", req.PageSize))
+	return c.client.ListUsers(ctx, req)
+}
+
+func (c *userClient) Register(ctx context.Context, req *pb.RegisterRequest) (*pb.RegisterResponse, error) {
+	ctx, span := c.tracer.Start(ctx, "UserClient.Register")
+	defer span.End()
+
+	span.SetAttributes(
+		attribute.String("user.email", req.Email),
+		attribute.String("user.username", req.Username),
+	)
+	c.logger.Debug("Registering user", zap.String("email", req.Email))
+	return c.client.Register(ctx, req)
+}
+
+func (c *userClient) Login(ctx context.Context, req *pb.LoginRequest) (*pb.LoginResponse, error) {
+	ctx, span := c.tracer.Start(ctx, "UserClient.Login")
+	defer span.End()
+
+	span.SetAttributes(attribute.String("user.email", req.Email))
+	c.logger.Debug("User login", zap.String("email", req.Email))
+	return c.client.Login(ctx, req)
+}
+
+func (c *userClient) ValidateToken(ctx context.Context, req *pb.ValidateTokenRequest) (*pb.ValidateTokenResponse, error) {
+	ctx, span := c.tracer.Start(ctx, "UserClient.ValidateToken")
+	defer span.End()
+
+	c.logger.Debug("Validating token")
+	return c.client.ValidateToken(ctx, req)
+}
+
+func (c *userClient) Close() error {
+	c.logger.Info("Closing user client connection")
+	return c.conn.Close()
+}
